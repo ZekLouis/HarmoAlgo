34d12f4c6cfcc20f5e89265320eac9bf12b00eb7
Romain Chemartin <romain.chemartin@gmail.com>
2017-09-18 08:03:43 +0200

    ajout interface + classe Personne

---
+++ HarmoAlgoUIFX/src/carnet/MainApp.java
@@ -0,0 +1,87 @@
+package carnet;
+
+import java.io.IOException;
+
+import carnet.model.Personne;
+import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    // TEST ObservableList
+    private ObservableList<Personne> personnes = FXCollections.observableArrayList();
+
+    
+    public MainApp() {
+    	personnes.add(new Personne("Louis", "Gaume", "Rue rude"));
+    	personnes.add(new Personne("Romain", "Chemartin", "Rue du masan"));
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/Root.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/Home.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ HarmoAlgoUIFX/src/carnet/model/Personne.java
@@ -0,0 +1,60 @@
+package carnet.model;
+
+import java.io.Serializable;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Personne implements Serializable{
+	private StringProperty nom;
+	private StringProperty prenom;
+	private StringProperty adresse;
+
+	public Personne(String nom, String prenom, String adresse) {
+		this.nom = new SimpleStringProperty(nom);
+		this.prenom = new SimpleStringProperty(prenom);
+		this.adresse = new SimpleStringProperty(adresse);
+	}
+
+	public String getNom() {
+		return nom.get();
+	}
+
+	public StringProperty getNomProperty() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom.set(nom);
+	}
+
+	public String getPrenom() {
+		return prenom.get();
+	}
+	
+	public StringProperty getPrenomProperty() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom.set(prenom);
+	}
+
+	public String getAdresse() {
+		return adresse.get();
+	}
+	
+	public StringProperty getAdresseProperty() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse.set(adresse);
+	}
+
+	@Override
+	public String toString() {
+		return "Personne [nom=" + nom + ", prenom=" + prenom + ", adresse=" + adresse + "]";
+	}
+	
+}
