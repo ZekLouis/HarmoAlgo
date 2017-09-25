3407a67858dc2797d473788d4e39de833958109d
ZekLouis <louis.gaume@etu.unilim.fr>
2017-09-25 11:20:16 +0200

    up

--- HarmoAlgoPaint/src/paint/view/HomeController.java
+++ HarmoAlgoPaint/src/paint/view/HomeController.java
@@ -78,84 +78,9 @@ public class HomeController {
     private void initialize() {
     }
     
-    private void initDrawEvents2() {
-    		rectBound.setFill(Color.TRANSPARENT);
-        rectBound.setStroke(Color.GOLD);
-        ScrollPane scp = new ScrollPane();
-        HBox root = new HBox(15);
-        scp.setContent(root);
-       
-
-        ///////////////////////////////////////////////
-        anchorPaneViewParent.addEventFilter(MouseEvent.ANY, new EventHandler<MouseEvent>() {
-           @Override
-               public void handle(MouseEvent event) {
-
-                   if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-                       if (rectBound.getParent() == null) {
-                           rectBound.setWidth(0.0); rectBound.setHeight(0.0);
-                           rectBound.setLayoutX(event.getX()); rectBound.setLayoutY(event.getY()); // setX or setY
-                           anchorPaneViewParent.getChildren().add(rectBound);
-                       }
-                   } else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
-
-                   } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-                       rectBound.setWidth(event.getX() - rectBound.getLayoutX());
-                       rectBound.setHeight(event.getY() - rectBound.getLayoutY());
-                   } else if (event.getEventType() == MouseEvent.MOUSE_CLICKED
-                           && event.getButton() == MouseButton.SECONDARY) {
-                       if (rectBound.getParent() != null) {
-                    	   	anchorPaneViewParent.getChildren().remove(rectBound);
-                       }
-                   } else if (event.getEventType() == MouseEvent.MOUSE_CLICKED
-                           && event.getButton() == MouseButton.PRIMARY && event.getClickCount() > 1) {
-                       //////////////// i crop here //////////////
-                       /*PixelReader reader = imageView1.getImage().getPixelReader();
-                       WritableImage newImage = new WritableImage(reader, (int) rectBound.getLayoutX(),
-                               (int) rectBound.getLayoutY(),
-                               (int) rectBound.getWidth(),
-                               (int) rectBound.getHeight());
-
-                       root.getChildren().add(new ImageView(newImage));*/
-                   }
-
-               }
-       });
-        //////////////////////////////////////////////
-        root.getChildren().add(anchorPaneViewParent);
-    }
-    
-    private void initDrawEvents() {
-        final double maxX = imageView.getImage().getWidth();
-        final double maxY = imageView.getImage().getHeight();
-        //imageView.setVisible(false);
-        //AnchorPane anchorRoot = anchorPaneViewParent;
-
-        
-        imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent me) {
-                System.out.println("Dragged, x:" + me.getSceneX() + " y:" + me.getSceneY());
-            		Rectangle rect = new Rectangle(me.getX(),me.getY(),100,100);
-            		rect.setFill(Color.RED);
-            		rect.setStroke(Color.RED);
-            		rect.setStrokeWidth(2);
-                /*Line line = new Line(initX, initY, me.getSceneX(), me.getSceneY());
-                line.setFill(null);
-                line.setStroke(Color.RED);
-                line.setStrokeWidth(2);*/
-            		rect.setVisible(true);
-            		anchorPaneViewParent.getChildren().add(rect);
-
-            }
-        });
-    }
-    
-    
-    
     @FXML
     private void handleCharger() {
-    		FileChooser fileChooser = new FileChooser();
+		FileChooser fileChooser = new FileChooser();
 
         FileChooser.ExtensionFilter extFilterJpg = new FileChooser.ExtensionFilter("Images (*.jpg)", "*.jpg");
         fileChooser.getExtensionFilters().add(extFilterJpg);
@@ -190,14 +115,14 @@ public class HomeController {
         });
 
         if (file != null) {
-        		image = new Image(file.toURI().toString());
-        		imageView.setImage(image);
-        		mainApp.setNomFenetre(file.getName());
+    		image = new Image(file.toURI().toString());
+    		imageView.setImage(image);
+    		mainApp.setNomFenetre(file.getName());
             imageView.setFitHeight(scrollPane.getHeight());
             imageView.setFitWidth(scrollPane.getWidth());
             imageView.setPreserveRatio(true);
             scrollPane.setContent(imageView);
-            //scrollPane.setPannable(true);
+            scrollPane.setPannable(true);
             
             // reset color effect
             if(imageView.getEffect() == null) {
@@ -216,19 +141,17 @@ public class HomeController {
     
     @FXML
     public void handleBlackAndWhite() {
-    		
-    		
-    		if(imageView.getEffect() == null) {
-        		grayscale = new ColorAdjust();
-    		} else {
-    			grayscale = (ColorAdjust) imageView.getEffect();
-    		}
-    		
-    		if (grayscale.getSaturation() == 0.0) {
-    			grayscale.setSaturation(-1.0);
-    		} else {
-    			grayscale.setSaturation(0.0);
-    		}
+		if(imageView.getEffect() == null) {
+    		grayscale = new ColorAdjust();
+		} else {
+			grayscale = (ColorAdjust) imageView.getEffect();
+		}
+		
+		if (grayscale.getSaturation() == 0.0) {
+			grayscale.setSaturation(-1.0);
+		} else {
+			grayscale.setSaturation(0.0);
+		}
         imageView.setEffect(grayscale);
     }
     
@@ -243,27 +166,25 @@ public class HomeController {
     
     @FXML
     private void handleSave() {
-	    	FileChooser fileChooser = new FileChooser();
-	    	fileChooser.setInitialFileName(fileName);
-	    	fileChooser.setTitle("Sauvegarder l'image");
-	    	File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
-	    	Image image = imageView.snapshot(null, null);
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setInitialFileName(fileName);
+    	fileChooser.setTitle("Sauvegarder l'image");
+    	FileChooser.ExtensionFilter extFilterJpg = new FileChooser.ExtensionFilter("Images (*.jpg)", "*.jpg");
+        fileChooser.getExtensionFilters().add(extFilterJpg);
+        
+        FileChooser.ExtensionFilter extFilterPng = new FileChooser.ExtensionFilter("Images (*.png)", "*.png");
+        fileChooser.getExtensionFilters().add(extFilterPng);
+        
+        FileChooser.ExtensionFilter extFilterJpeg = new FileChooser.ExtensionFilter("Images (*.jpeg)", "*.jpeg");
+        fileChooser.getExtensionFilters().add(extFilterJpeg);
+    	File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
+    	Image image = imageView.snapshot(null, null);
 	    	
-	    	try {
+    	try {
 			ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
-    
-    @FXML
-    private void handleDragEntered() {
-    		System.out.println("drag entered");
-    }
-    
-    @FXML
-    private void handleDragExited() {
-    		System.out.println("drag exited");
     }
 
     /**
